f2cec0df69c93e744983a77d9e1304ff88369c9f
Vasil Nunev <vasil.nunev@pagesuite.com>
2017-01-26 17:06:48 +0000

    add "make suggestion" in the about page

--- Pixtic/AboutViewController.swift
+++ Pixtic/AboutViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
     
     @IBOutlet weak var versionLbl: UILabel!
@@ -16,8 +17,6 @@ class AboutViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
         versionLbl.text = "Version \(Bundle.main.infoDictionary!["CFBundleShortVersionString"]!)"
     }
@@ -27,7 +26,24 @@ class AboutViewController: UIViewController {
     }
     
     @IBAction func suggestionPressed(_ sender: Any) {
+        let mailComposeViewController = configureMailComposer()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        }else {
+            let alert = UIAlertController(title: "Could not send email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func configureMailComposer() -> MFMailComposeViewController {
+        let mailVC = MFMailComposeViewController()
+        mailVC.mailComposeDelegate = self
+        mailVC.setToRecipients(["pickpixapp@gmail.com"])
+        mailVC.setSubject("PickPix App Feedback")
         
+        return mailVC
     }
     
     @IBAction func removeAdsPressed(_ sender: Any) {
@@ -40,4 +56,9 @@ class AboutViewController: UIViewController {
         }
     }
     
+    
+    //MARK: MFMAilComposeViewControllerDelegate Method
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
